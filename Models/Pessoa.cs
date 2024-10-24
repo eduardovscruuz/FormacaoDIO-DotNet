90ac15313b9d194f659c87540a9ea0768c85628d
eduardovscruuz <eduardovscruuz@gmail.com>
2024-10-24 13:56:22 -0300

    commit inicial

---
+++ Models/Pessoa.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public virtual void Apresentar()
+        {
+            Console.WriteLine($"Olá, me chamo {Nome} e tenho {Idade} anos");
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ConceitosPOO.Models;
+
+/*------------------- AULA 3 -------------------*/
+
+Aluno a1 = new Aluno();
+a1.Nome = "Eduardo";
+a1.Idade = 24;
+a1.Nota = 8.5;
+
+a1.Apresentar();
+
+Professor pr1 = new Professor();
+pr1.Nome = "Leonardo";
+pr1.Idade = 30;
+pr1.Salario = 2500;
+
+pr1.Apresentar();
+Console.WriteLine("");
+
+/*------------------- AULA 2 -------------------*/
+
+ContaCorrente c1 = new ContaCorrente(123, 4000);
+
+/* c1.saldo = 5000; INACESSÍVEL DEVIDO ENCAPSULAMENTO */
+c1.ExbirSaldo();
+Console.WriteLine("");
+
+c1.Sacar(3000);
+c1.ExbirSaldo();
+Console.WriteLine("");
+
+c1.Sacar(2000);
+c1.ExbirSaldo();
+Console.WriteLine("");
+
+/*------------------- AULA 1 -------------------*/
+
+Pessoa pe1 = new Pessoa();
+
+pe1.Nome = "Eduardo";
+pe1.Idade = 24;
+
+pe1.Apresentar();
